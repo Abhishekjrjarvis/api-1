e30ddff9b400d10231426cb8dd21ca768c58c88f
Ankush Kumar Singh <63860698+anikushalam@users.noreply.github.com>
2022-01-20 19:49:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -234,10 +234,10 @@ app.post("/admin/:aid/reject/ins/:id", async (req, res) => {
 // Institute Admin Routes
 
 // Institute Creation
-//for global user admin "61e6b5542501b28fc6d70450"
+//for global user admin "61e96ed24b9172ff5234ba3e"
 //for local my system "61d83f55f9245740b77ddec3"
 app.post("/ins-register", async (req, res) => {
-  const admins = await Admin.findById({ _id: "61e6b5542501b28fc6d70450" });
+  const admins = await Admin.findById({ _id: "61e96ed24b9172ff5234ba3e" });
   const existInstitute = await InstituteAdmin.findOne({ name: req.body.name });
   const existAdmin = await Admin.findOne({ adminUserName: req.body.name });
   const existUser = await User.findOne({ username: req.body.name });
@@ -1818,7 +1818,7 @@ app.post("/department/holiday/:did", async (req, res) => {
 
 app.post("/user-register", async (req, res) => {
   const { username } = req.body;
-  const admins = await Admin.findById({ _id: "61e6b5542501b28fc6d70450" });
+  const admins = await Admin.findById({ _id: "61e96ed24b9172ff5234ba3e" });
   const existAdmin = await Admin.findOne({ adminUserName: username });
   const existInstitute = await InstituteAdmin.findOne({ name: username });
   const existUser = await User.findOne({ username: username });
